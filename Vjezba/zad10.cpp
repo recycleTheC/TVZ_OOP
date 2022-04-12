6baba21d756fd1d29832d1e7fb4f124cfa1bb99b
recycleTheC <mario.kopjar1@gmail.com>
2022-04-12 17:19:14 +0200

    Vježba (10. zadatak)

---
+++ Vjezba/zad10.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<vector>
+#include <string>
+
+using namespace std;
+
+struct Ucenik {
+	string ime, prezime;
+};
+
+class Razred {
+private:
+	void dealocirajUcenike() {
+		for (int i = 0; i < ucenik.size(); i++)
+		{
+			delete ucenik[i];
+		}
+
+		ucenik.clear();
+	}
+public:
+	vector<Ucenik*> ucenik;
+	float prosjecnaOcjena = 0;
+
+	Razred() {}
+	Razred(vector<Ucenik*>& ucenici) {
+		for (int i = 0; i < ucenici.size(); i++)
+		{
+			Ucenik* novi = new Ucenik;
+			*novi = *ucenici[i];
+			this->ucenik.push_back(novi);
+		}
+	}
+	~Razred() {
+		dealocirajUcenike();
+	}
+
+	Razred& operator = (Razred&& r) { // operator prijenosa
+
+		this->prosjecnaOcjena = r.prosjecnaOcjena;
+		this->dealocirajUcenike();
+
+		for (int i = 0; i < r.ucenik.size(); i++)
+		{
+			ucenik.push_back(r.ucenik[i]);
+			r.ucenik[i] = NULL;
+		}
+
+		r.ucenik.clear();
+
+		return *this;
+	}
+
+	Razred& operator = (Razred& r) { // operator kopiranja
+
+		if (this != &r) {
+			this->prosjecnaOcjena = r.prosjecnaOcjena;
+			this->dealocirajUcenike();
+
+			for (int i = 0; i < r.ucenik.size(); i++)
+			{
+				Ucenik* tmp = new Ucenik;
+				*tmp = *r.ucenik[i];
+				ucenik.push_back(tmp);
+			}
+		}
+
+		return *this;
+	}
+
+	Razred(const Razred& r) { // kopirni konstruktor
+
+		this->prosjecnaOcjena = r.prosjecnaOcjena;
+		this->dealocirajUcenike();
+
+		for (int i = 0; i < r.ucenik.size(); i++)
+		{
+			Ucenik* tmp = new Ucenik;
+			*tmp = *r.ucenik[i];
+			ucenik.push_back(tmp);
+		}
+	}
+
+	Razred(Razred&& r) { // kopirni konstruktor s prijenosom
+
+		this->prosjecnaOcjena = r.prosjecnaOcjena;
+		this->dealocirajUcenike();
+
+		for (int i = 0; i < r.ucenik.size(); i++)
+		{
+			ucenik.push_back(r.ucenik[i]);
+			r.ucenik[i] = NULL;
+		}
+
+		r.ucenik.clear();
+	}
+};
+
+int main() {
+	vector<Ucenik*> ucenici;
+
+	for (int i = 0; i < 3; i++)
+	{
+		ucenici.push_back(new Ucenik);
+		ucenici[i]->ime = "Ucenik " + to_string(i + 1);
+	}
+
+	Razred razred1(ucenici);
+	razred1.ucenik[0]->ime = "Ucenik A";
+
+	Razred razred2(razred1);
+	razred2.ucenik[0]->ime = "Ucenik B";
+
+	Razred razred3;
+	razred3 = razred2;
+	razred3.ucenik[0]->ime = "Ucenik C";
+
+	Razred razred4 = move(Razred(ucenici));
+	razred4.ucenik[0]->ime = "Ucenik D";
+
+	Razred razred5;
+	razred5 = Razred(ucenici);
+	razred5.ucenik[0]->ime = "Ucenik E";
+
+	return 0;
+}
